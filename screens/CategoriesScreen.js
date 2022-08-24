e9fb8095c5ae46cb4474a410e2ef4de84044ec1c
Nhan Phan <kippulainenko@gmail.com>
2022-08-24 23:07:51 +0300

    use navigation.navigatein grid element

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,17 +3,28 @@ import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data"
 
 
-const renderCategoryItem = (itemData) => (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
-)
 
-const CategoriesScreen = () => (
+
+const CategoriesScreen = ({ navigation }) => {
+    const pressHandler = () => {
+        navigation.navigate('Meals Overview')
+    }
+
+    const renderCategoryItem = (itemData) => {
+        return (
+        <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler}/>
+        )
+    }
+
+
+    return (
     <FlatList 
     data={CATEGORIES}
     renderItem={renderCategoryItem}
     keyExtractor={(item) => item.id}
     numColumns={2}
     />
-)
+    )
+}
 
 export default CategoriesScreen
